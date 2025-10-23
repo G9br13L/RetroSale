25499b1f20c8ceb1a8db2d7a6e4036c66393129e
Miro <gabriel.miro.velasco03@gmail.com>
2025-10-23 13:37:08 +0800

    fix(dashboard page): fix controller

--- backend/app/Controllers/Admin.php
+++ backend/app/Controllers/Admin.php
@@ -28,7 +28,7 @@ class Admin extends BaseController
         }
 
         // Load the dashboard view with data
-        return view('admin/dashboardadmin', [
+        return view('/admin/dashboardadmin', [
             'requestsCount' => $requestsCount,
             'servicesCount' => $servicesCount,
         ]);
